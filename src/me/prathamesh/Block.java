979982dc984fecbf9d39bdadce1c81ce8fd6d1af
prathamesh-a <89336149+prathamesh-a@users.noreply.github.com>
2022-02-09 14:01:34 +0530

    update

--- src/me/prathamesh/Block.java
+++ src/me/prathamesh/Block.java
@@ -1,5 +1,18 @@
 package me.prathamesh;
 
+import java.util.Date;
+
 public class Block {
+    public String hash;
+    public String previousHash;
+    private String data;
+    private long timeStamp;
+
+    public Block(String data, String previousHash){
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+    }
+
 
 }
--- src/me/prathamesh/StringUtils.java
+++ src/me/prathamesh/StringUtils.java
@@ -1,4 +1,22 @@
 package me.prathamesh;
 
+import java.security.MessageDigest;
+
 public class StringUtils {
+    public static String applyHash(String input){
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(input.getBytes());
+            StringBuffer hexString = new StringBuffer();
+            for (int i=0; i<hash.length; i++){
+                String hex = Integer.toHexString(0xff & hash[i]);
+                if (hex.length()==1) hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
 }
